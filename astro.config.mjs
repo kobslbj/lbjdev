e72d061cc2c942d3368576e728d2516fa827ebb4
Justin Li <kobs666666@gmail.com>
2024-06-30 23:05:30 +0800

    chore: Add partytown integration to astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import partytown from '@astrojs/partytown'
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), preact()],
+  integrations: [react(), tailwind(), preact(),partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   site: "https://kobslbj.dev"
 });
